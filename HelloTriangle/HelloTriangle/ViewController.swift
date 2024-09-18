361987ff498955c1ed7bd2b59ea7d91195a6e341
yiniu <>
2024-09-18 22:29:23 +0800

    Init Hello Triangle

---
+++ HelloTriangle/HelloTriangle/ViewController.swift
@@ -0,0 +1,14 @@
+
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
+
