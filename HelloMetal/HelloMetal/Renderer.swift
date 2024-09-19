b28862636f408b61fa61163b85cadd87e68941a5
yiniu <>
2024-09-19 22:54:19 +0800

    Hello Triangle(Completed)

--- HelloMetal/HelloMetal/Renderer.swift
+++ HelloMetal/HelloMetal/Renderer.swift
@@ -2,16 +2,39 @@
 import Foundation
 import MetalKit
 import Metal
+import simd
+
+struct Vertext {
+    var position: SIMD4<Float>
+    var color: SIMD4<Float>
+}
+
 class Renderer: NSObject {
     unowned var mtkView: MTKView
     private var device: MTLDevice!
     private var commandQueue: MTLCommandQueue!
-    
+    private var pipelineState: MTLRenderPipelineState!
     
     init(mtkView: MTKView) {
         self.mtkView = mtkView
         device = mtkView.device
         commandQueue = device.makeCommandQueue()
+        
+        let library = device.makeDefaultLibrary()
+        let vertextFunc = library?.makeFunction(name: "vertexShader")
+        let fragmentFunc = library?.makeFunction(name: "fragmentShader")
+        
+        let pipelineDescriptor = MTLRenderPipelineDescriptor()
+        pipelineDescriptor.vertexFunction = vertextFunc
+        pipelineDescriptor.fragmentFunction = fragmentFunc
+        pipelineDescriptor.colorAttachments[0].pixelFormat = mtkView.colorPixelFormat;
+        
+        do {
+            pipelineState = try device.makeRenderPipelineState(descriptor: pipelineDescriptor)
+        } catch {
+            fatalError("fail to create pipelineState")
+        }
+
         super.init()
     }
     
@@ -25,6 +48,22 @@ extension Renderer: MTKViewDelegate {
         }
         let commanderBuffer = commandQueue.makeCommandBuffer()
         let commanderEncoder = commanderBuffer?.makeRenderCommandEncoder(descriptor: renderPassDesriptor)
+        commanderEncoder?.setRenderPipelineState(pipelineState)
+        
+        let vertices = [
+            //左下角
+            Vertext(position: [-1.0, -1.0, 1.0, 1.0], color: [1.0, 0.0, 0.0, 1.0]),
+            //正上方
+            Vertext(position: [ 0.0,  1.0, 1.0, 1.0], color: [1.0, 0.5, 0.0, 1.0]),
+            //右下角
+            Vertext(position: [ 1.0, -1.0, 1.0, 1.0], color: [0.0, 1.0, 0.0, 1.0]),
+        ]
+        
+        let vertexBuffer = device.makeBuffer(bytes: vertices, length: MemoryLayout<Vertext>.stride * vertices.count)
+        commanderEncoder?.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+        
+        commanderEncoder?.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: vertices.count)
+        
         commanderEncoder?.endEncoding()
         
         guard let drawable = view.currentDrawable else {
@@ -32,7 +71,6 @@ extension Renderer: MTKViewDelegate {
         }
         commanderBuffer?.present(drawable)
         commanderBuffer?.commit()
-        commanderBuffer?.waitUntilCompleted()
     }
     
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
--- HelloTriangle/HelloTriangle/ViewController.swift
+++ HelloTriangle/HelloTriangle/ViewController.swift
@@ -2,13 +2,31 @@
 
 import Cocoa
 
+import Cocoa
+import MetalKit
+
 class ViewController: NSViewController {
+    
+    var render: Renderer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        guard let mtkView = view as? MTKView else {
+            fatalError("Please use the MTKView")
+        }
+        
+        mtkView.device = MTLCreateSystemDefaultDevice()
+        // 背景颜色
+        mtkView.clearColor = MTLClearColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        mtkView.enableSetNeedsDisplay = true
+        
+        render = Renderer(mtkView: mtkView)
+        mtkView.delegate = render
     }
-
+    
+    
 }
 
